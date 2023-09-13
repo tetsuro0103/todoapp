fd931e1f904e9a363370ccec54a73c1be80bd673
泉徹郎 <>
2023-09-13 13:20:54 +0900

    ユーザ登録、ログイン、ログアウト実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def avatar_image
+    'default-avatar.png'
+  end
+
 end
